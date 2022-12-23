3a5112dc3f79aa9861330fe27fe43fa3fc5616b8
Cookie <85786140+Cookie755233@users.noreply.github.com>
2022-12-23 23:09:38 +0800

    what is this

--- msg/reply.py
+++ msg/reply.py
@@ -6,7 +6,12 @@ def read_user_message(user_message: str):
     if re.match('hello', user_message):
         return 'Hi! what can i help'
     
+    if re.match('bye', user_message):
+        return 'Cya!'
+
     if re.match('title', user_message):
         df = read_mongo()
         
         return ','.join(df.columns)
+    
+    return
