86461adf16518c7d0c02c2913bbdb54ae75a7015
Cookie <85786140+Cookie755233@users.noreply.github.com>
2022-12-23 22:38:33 +0800

    change false import

--- api/index.py
+++ api/index.py
@@ -3,7 +3,6 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-from db.mongo import count_docs
 from msg.reply import read_user_message
 import os
 
