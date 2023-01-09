fa14b23c50ff0c229775db48d080874567b987d1
Cookie <85786140+Cookie755233@users.noreply.github.com>
2023-01-10 00:15:52 +0800

    helper

---
+++ utils/helper.py
@@ -0,0 +1,10 @@
+
+
+def pairwise(iterable):
+    iterable = iter(iterable)
+    while True:
+        try:
+            yield next(iterable), next(iterable)
+        except StopIteration:
+            # no more elements in the iterator
+            return
