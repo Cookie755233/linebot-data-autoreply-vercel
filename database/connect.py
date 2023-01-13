89abc3c8870c3bcbe0f637c41f18cfeb9284bdc1
Cookie755233 <85786140+Cookie755233@users.noreply.github.com>
2023-01-13 17:54:05 +0800

    beta 1

---
+++ database/connect.py
@@ -0,0 +1,13 @@
+
+from pymongo import MongoClient
+import os
+
+USERNAME = os.getenv("MONGODB_USERNAME")
+PASSWORD = os.getenv("MONGODB_PASSWORD")
+
+def connect_mongo(username=USERNAME,
+                   password=PASSWORD) -> MongoClient:
+    mongo_uri = f"mongodb+srv://{username}:{password}@cluster0.xveirqo.mongodb.net/test?retryWrites=true&w=majority"
+    client = MongoClient(mongo_uri)
+
+    return client
